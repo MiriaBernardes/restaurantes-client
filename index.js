93294c7eaa7b93640cda89c0e92489de2450081b
Miriã Bernardes <miriabernardes@pop-os.localdomain>
2020-07-09 18:07:06 -0300

    cliente teste para restaurantes-api

---
+++ index.js
@@ -0,0 +1,31 @@
+const axios = require('axios')
+
+
+var nome = 'Top100'
+
+var restaurantes = [];
+
+restaurantes.push({nome: 'top20'}, {nome: 'top200'}, {nome: 'top2000'})
+
+axios.post('http://localhost:3000/restaurantes', restaurantes)
+.then((resposta) => {
+    console.log(resposta.data)
+})
+
+axios.get('http://localhost:3000/restaurantes')
+.then((resposta) => {
+    console.log(resposta.data)
+})
+
+axios.put('http://localhost:3000/restaurante/' + 'top20', {nome: 'top21'})
+.then((resp) => {
+    console.log(resp.data);
+}).catch((erro) => {
+    console.log(erro)
+})
+
+
+axios.delete('http://localhost:3000/restaurante/' + 'top20')
+.then((resposta) => {
+    console.log(resposta.data)
+})
